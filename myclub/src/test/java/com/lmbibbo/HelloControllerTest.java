78fe806594e42671ba34d5979477882c10d90f5a
Luis Mariano Bibo <lmbibbo@gmail.com>
2022-07-27 22:12:07 -0300

    StarUp

--- myclub/src/test/java/com/lmbibbo/HelloControllerTest.java
+++ myclub/src/test/java/com/lmbibbo/HelloControllerTest.java
@@ -5,8 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -15,10 +13,7 @@ import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
-@IntegrationTest({"server.port:0",
-        "spring.datasource.url:jdbc:h2:mem:myclub;DB_CLOSE_ON_EXIT=FALSE"})
 public class HelloControllerTest {
     @Value("${local.server.port}")
     int port;
